3db3e2e23ff613d4bf1831db95e9a1731eb717c8
MMMM2000 <148327739+MMMM2000@users.noreply.github.com>
2025-08-05 15:18:55 +0200

    Restore modular logger UIs

--- data_logger/logger_ui_modern_a.py
+++ data_logger/logger_ui_modern_a.py
@@ -1,4 +1,4 @@
-"""Default modern UI for the data logger.
+"""Alternative modern UI option A for the data logger.
 
 This layout uses vertical stacking with spacious margins and a solid
 background for a contemporary look. It mirrors the controls of the original
@@ -7,9 +7,11 @@ interface while avoiding fixed geometries so widgets cannot overlap.
 
 from PyQt6 import QtCore, QtGui, QtWidgets
 
+from .data_logger import FileNameBuilderWidget
 
-class Ui_MainWindow(object):
-    """Simple vertical layout used as the default UI."""
+
+class UiMainWindowModernA(object):
+    """Simple vertical layout."""
 
     def setupUi(self, MainWindow: QtWidgets.QMainWindow) -> None:
         MainWindow.setObjectName("MainWindowModernA")
@@ -32,8 +34,6 @@ class Ui_MainWindow(object):
         serial_layout.addWidget(QtWidgets.QLabel("Port:"))
         self.comboBox_port = QtWidgets.QComboBox()
         serial_layout.addWidget(self.comboBox_port)
-        self.pushButton_refresh_ports = QtWidgets.QPushButton("Refresh")
-        serial_layout.addWidget(self.pushButton_refresh_ports)
         serial_layout.addWidget(QtWidgets.QLabel("Baud:"))
         self.comboBox_baud = QtWidgets.QComboBox()
         self.comboBox_baud.addItems([
@@ -64,7 +64,6 @@ class Ui_MainWindow(object):
         main_layout.addWidget(self.groupBox_cmd)
 
         # --- Logging controls ------------------------------------------------
-        from .data_logger import FileNameBuilderWidget
         self.groupBox_log = QtWidgets.QGroupBox("Logging")
         log_layout = QtWidgets.QGridLayout(self.groupBox_log)
 
@@ -92,24 +91,21 @@ class Ui_MainWindow(object):
         log_layout.addWidget(self.spinBox_log_sample_count, 2, 1)
         log_layout.addWidget(self.label_samples, 2, 2)
 
-        self.label_time_estimate = QtWidgets.QLabel("Est. time: N/A")
-        log_layout.addWidget(self.label_time_estimate, 3, 0, 1, 3)
-
         self.progressBar_logging = QtWidgets.QProgressBar()
         self.pushButton_cancel = QtWidgets.QPushButton("Cancel")
         self.checkBox_subdir = QtWidgets.QCheckBox("Use subfolder")
-        log_layout.addWidget(self.progressBar_logging, 4, 0, 1, 2)
-        log_layout.addWidget(self.pushButton_cancel, 4, 2)
-        log_layout.addWidget(self.checkBox_subdir, 4, 3)
+        log_layout.addWidget(self.progressBar_logging, 3, 0, 1, 2)
+        log_layout.addWidget(self.pushButton_cancel, 3, 2)
+        log_layout.addWidget(self.checkBox_subdir, 3, 3)
 
         self.file_name_builder = FileNameBuilderWidget(self.groupBox_log, self.lineEdit_log_file)
-        log_layout.addWidget(self.file_name_builder, 5, 0, 1, 4)
+        log_layout.addWidget(self.file_name_builder, 4, 0, 1, 4)
 
         main_layout.addWidget(self.groupBox_log)
 
         # --- Status bar ------------------------------------------------------
         self.statusbar = QtWidgets.QStatusBar(MainWindow)
-        self.label_connection_indicator = QtWidgets.QLabel("● Disconnected")
+        self.label_connection_indicator = QtWidgets.QLabel("\u25cf Disconnected")
         self.label_connection_indicator.setStyleSheet("color: red;")
         self.statusbar.addPermanentWidget(self.label_connection_indicator)
         self.pushButton_switch_ui = QtWidgets.QPushButton("Switch UI")
@@ -122,4 +118,3 @@ class Ui_MainWindow(object):
         self.pushButton_connect_port = self.pushButton_connect
 
         QtCore.QMetaObject.connectSlotsByName(MainWindow)
-
